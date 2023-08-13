2569793977db2d5ae1bd04b3c03fce22f6dbfd09
Luis Puc <Luis.Puc@asd-tech.mx>
2023-08-13 16:51:53 -0600

    Btn Component: btn color customize by inputs

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+class BtnColor{
+  static readonly SUCCESS = 'success';
+  static readonly PRIMARY = 'primary';
+}
+
 @Component({
   selector: 'app-btn',
   templateUrl: './btn.component.html',
-  styleUrls: ['./btn.component.css']
+  styleUrls: []
 })
 export class BtnComponent implements OnInit {
 
   @Input() btnType: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color: 'success' | 'primary' = 'primary';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get colors(){
+    return {
+      'bg-success-700': this.color == BtnColor.SUCCESS,
+      'hover:bg-success-800': this.color == BtnColor.SUCCESS,
+      'focus:ring-success-300': this.color == BtnColor.SUCCESS,
+      'bg-primary-700': this.color == BtnColor.PRIMARY,
+      'hover:bg-primary-800': this.color == BtnColor.PRIMARY,
+      'focus:ring-primary-300': this.color == BtnColor.PRIMARY
+    }
   }
 
 }
